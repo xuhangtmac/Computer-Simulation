7895cbeb0134b5c6cdf7a9ed1057390ef1cd8599
xuhangtmac <xuhangtmac@brandeis.edu>
2018-01-25 14:24:23 -0500

    First

---
+++ Untitled.R
@@ -0,0 +1,4 @@
+housing.df <- read.csv("WestRoxbury.csv", header = TRUE)  # load data
+dim(housing.df)  # find the dimension of data frame
+head(housing.df)  # show the first six rows
+View(housing.df)  # show all the data in a new tab
